fbd716d321434f3f6e0762b744a4ccef712cfad4
TeddyOdesign <teddyobrien@aviminfra.com>
2025-07-19 13:38:57 -0500

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,1 +1,16 @@
+const { getDefaultConfig } = require('expo/metro-config');
 
+const config = getDefaultConfig(__dirname);
+
+// Add support for additional file extensions
+config.resolver.assetExts.push(
+  // Adds support for `.db` files for SQLite databases
+  'db'
+);
+
+// Add support for Mapbox GL JS
+config.resolver.alias = {
+  '@mapbox/mapbox-gl-style-spec': '@mapbox/mapbox-gl-style-spec/dist/index.cjs',
+};
+
+module.exports = config;
